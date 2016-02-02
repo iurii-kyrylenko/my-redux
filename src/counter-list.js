7ee7eb8a4116b01c43998ef81b84949bccebbbb0
Iurii Kyrylenko <iurii_kyrylenko@epam.com>
2016-02-02 20:52:02 +0200

    My React CounterListApp Example

--- src/counter-list.js
+++ src/counter-list.js
@@ -27,55 +27,128 @@ function CounterListApp() {
         ];
     };
 
-    const testAddCounter = () => {
-        const listBefore = [];
-        const listAfter = [0];
+    const counterList = (state = [], action) => {
+        switch (action.type) {
+            case 'ADD_COUNTER':
+                return addCounter(state);
+            case 'REMOVE_COUNTER':
+                if(!state.length) return state;
+                return removeCounter(state, state.length - 1);
+            case 'INCREMENT':
+                return incrementCounter(state, action.index);
+            case 'DECREMENT':
+                return decrementCounter(state, action.index);
+            default:
+                return state;
+        }
+    }
 
-        deepFreeze(listBefore);
+    const { createStore } = Redux;
+    const store = createStore(counterList);
 
-        expect(
-            addCounter(listBefore)
-        ).toEqual(listAfter);
+    const Counter = ({
+        index,
+        value,
+        onIncrement,
+        onDecrement
+    }) => (
+        <p>
+            <button onClick={onIncrement}>+</button>
+            &nbsp;&nbsp;
+            <button onClick={onDecrement}>-</button>
+            &nbsp;&nbsp;
+            <span>{value}</span>
+        </p>
+    );
+
+    const CounterList = ({
+        value,
+        onAddCounter,
+        onRemoveCounter
+    }) => (
+        <div>
+            <button onClick={onAddCounter}>Add Counter</button>
+            &nbsp;&nbsp;
+            <button onClick={onRemoveCounter}>Remove Counter</button>
+            <h2>Counter Number: {store.getState().length}</h2>
+            <div>
+                {value.map((counter, id) =>
+                    <Counter
+                        key={id}
+                        index={id}
+                        value={counter}
+                        onIncrement={() => store.dispatch({type: 'INCREMENT', index: id})}
+                        onDecrement={() => store.dispatch({type: 'DECREMENT', index: id})}>
+                    </Counter>
+                )}
+            </div>
+        </div>
+    );
+
+    const render = () => {
+        ReactDOM.render(
+            <CounterList
+                value={store.getState()}
+                onAddCounter={() => store.dispatch({type: 'ADD_COUNTER'})}
+                onRemoveCounter={() => store.dispatch({type: 'REMOVE_COUNTER'})}>
+            </CounterList>,
+            document.getElementById('root')
+        );
     };
 
-    const testRemoveCounter = () => {
-        const listBefore = [0, 10, 20];
-        const listAfter = [0, 20];
+    render();
+    store.subscribe(render);
 
-        deepFreeze(listBefore);
-
+/****
+    const testStore = () => {
+        store.dispatch({type:'ADD_COUNTER'});
         expect(
-            removeCounter(listBefore, 1)
-        ).toEqual(listAfter);
+            store.getState()
+        ).toEqual([0]);
+
+        store.dispatch({type:'ADD_COUNTER'});
+        expect(
+            store.getState()
+        ).toEqual([0, 0]);
+
+        store.dispatch({type:'INCREMENT', index: 0});
+        expect(
+            store.getState()
+        ).toEqual([1, 0]);
+
+        store.dispatch({type:'INCREMENT', index: 1});
+        expect(
+            store.getState()
+        ).toEqual([1, 1]);
+
+        store.dispatch({type:'DECREMENT', index: 0});
+        expect(
+            store.getState()
+        ).toEqual([0, 1]);
+
+        store.dispatch({type:'DECREMENT', index: 1});
+        expect(
+            store.getState()
+        ).toEqual([0, 0]);
+
+        store.dispatch({type:'REMOVE_COUNTER'});
+        expect(
+            store.getState()
+        ).toEqual([0]);
+
+        store.dispatch({type:'REMOVE_COUNTER'});
+        expect(
+            store.getState()
+        ).toEqual([]);
+
+        store.dispatch({type:'REMOVE_COUNTER'});
+        expect(
+            store.getState()
+        ).toEqual([]);
+
+        console.log('All tests passed.');
     };
 
-    const testIncrementCounter = () => {
-        const listBefore = [0, 10, 20];
-        const listAfter = [0, 11, 20];
-
-        deepFreeze(listBefore);
-
-        expect(
-            incrementCounter(listBefore, 1)
-        ).toEqual(listAfter);
-    };
-
-    const testDecrementCounter = () => {
-        const listBefore = [0, 10, 20];
-        const listAfter = [0, 9, 20];
-
-        deepFreeze(listBefore);
-
-        expect(
-            decrementCounter(listBefore, 1)
-        ).toEqual(listAfter);
-    };
-
-    testAddCounter();
-    testRemoveCounter();
-    testIncrementCounter();
-    testDecrementCounter();
-
-    console.log('All tests passed.');
-
+    testStore();
+****/
 }
