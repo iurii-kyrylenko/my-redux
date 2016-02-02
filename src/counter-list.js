d65ce138ba0d10a5990d409ad176ac9de1aa31ba
Iurii Kyrylenko <iurii_kyrylenko@epam.com>
2016-02-02 17:11:30 +0200

    Added decrementCounter function

--- src/counter-list.js
+++ src/counter-list.js
@@ -19,6 +19,14 @@ function CounterListApp() {
         ];
     };
 
+    const decrementCounter = (list, index) => {
+        return [
+            ...list.slice(0, index),
+            list[index] - 1,
+            ...list.slice(index + 1)
+        ];
+    };
+
     const testAddCounter = () => {
         const listBefore = [];
         const listAfter = [0];
@@ -52,9 +60,21 @@ function CounterListApp() {
         ).toEqual(listAfter);
     };
 
+    const testDecrementCounter = () => {
+        const listBefore = [0, 10, 20];
+        const listAfter = [0, 9, 20];
+
+        deepFreeze(listBefore);
+
+        expect(
+            decrementCounter(listBefore, 1)
+        ).toEqual(listAfter);
+    };
+
     testAddCounter();
     testRemoveCounter();
     testIncrementCounter();
+    testDecrementCounter();
 
     console.log('All tests passed.');
 
