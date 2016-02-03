5b8db05125e4d158bd7b7c1de4802f3063efd3b0
Iurii Kyrylenko <iurii_kyrylenko@epam.com>
2016-02-03 16:24:18 +0200

    15 Reducer Composition with combineReducers()

--- src/todo.js
+++ src/todo.js
@@ -44,16 +44,15 @@ function TodoApp () {
         }
     };
 
-    const todoApp = (state = {}, action) => {
-        return {
-            todos: todos(state.todos, action),
-            visibilityFilter: visibilityFilter(state.visibilityFilter, action)
-        };
-    };
+    const { combineReducers } = Redux;
+
+    const todoApp = combineReducers({
+        todos,
+        visibilityFilter
+    });
 
     const { createStore } = Redux;
     const store = createStore(todoApp);
-
 
     const logState = () => {
         console.log(JSON.stringify(store.getState(), null, 2));
