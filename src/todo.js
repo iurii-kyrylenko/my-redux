21359a8d1ce6c88f18cae6f57fde129fd01fa354
Iurii Kyrylenko <iurii_kyrylenko@epam.com>
2016-02-03 12:40:14 +0200

    11-12 Writing a Todo List Reducer

--- src/todo.js
+++ src/todo.js
@@ -1,31 +1,85 @@
 function TodoApp () {
-    const toggleTodo = (todo) => {
-        // return Object.assign({}, todo, {completed: !todo.completed});
-        return {
-            ...todo,
-            completed: !todo.completed
-        };
+
+    const todos = (state = [], action) => {
+        switch(action.type) {
+            case 'ADD_TODO':
+                return [
+                    ...state,
+                    {
+                        id: action.id,
+                        text: action.text,
+                        completed: false
+                    }
+                ];
+            case 'TOGGLE_TODO':
+                return state.map((todo) => {
+                    if(action.id !== todo.id) {
+                        return todo;
+                    }
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    };
+                });
+            default:
+                return state;
+        }
     };
 
-    const testToggleTodo = () => {
-        const todoBefore = {
+    const testAddTodo = () => {
+        const action = {
+            type: 'ADD_TODO',
+            id: 0,
+            text: 'Learn Redux'
+        };
+        const stateBefore = [];
+        const stateAfter = [{
             id: 0,
             text: 'Learn Redux',
             completed: false
-        };
-        const todoAfter = {
-            id: 0,
-            text: 'Learn Redux',
-            completed: true
-        };
+        }];
 
-        deepFreeze(todoBefore);
+        deepFreeze(action);
+        deepFreeze(stateBefore);
 
         expect(
-            toggleTodo(todoBefore)
-        ).toEqual(todoAfter);
+            todos(stateBefore, action)
+        ).toEqual(stateAfter);
     }
 
+    const testToggleTodo = () => {
+        const action = {
+            type: 'TOGGLE_TODO',
+            id: 1
+        };
+        const stateBefore = [{
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        }, {
+            id: 1,
+            text: 'Watch Santa Barbara',
+            completed: false
+        }];
+        const stateAfter = [{
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        }, {
+            id: 1,
+            text: 'Watch Santa Barbara',
+            completed: true
+        }];
+
+        deepFreeze(action);
+        deepFreeze(stateBefore);
+
+        expect(
+            todos(stateBefore, action)
+        ).toEqual(stateAfter);
+    }
+
+    testAddTodo();
     testToggleTodo();
     console.log('All tests passed.');
 }
