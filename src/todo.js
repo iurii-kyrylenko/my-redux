4eac4a59b77af3cedebabb7e5f2ff00b362a6f90
Iurii Kyrylenko <iurii_kyrylenko@Iuriis-MacBook-Pro.local>
2016-02-07 16:59:36 +0200

    29 Generating Containers with connect() from React Redux (FilterLink)

--- src/todo.js
+++ src/todo.js
@@ -87,42 +87,31 @@ function TodoApp () {
         );
     };
 
-    class FilterLink extends Component {
-        componentDidMount() {
-            const {store} = this.context;
-            this.unsubscribe = store.subscribe(() =>
-                this.forceUpdate()
-            );
-        }
-
-        componentWillUnmount() {
-            this.unsubscribe();
-        }
-
-        render() {
-            const {store} = this.context;
-            const props = this.props;
-            const state = store.getState();
-
-            return (
-                <Link
-                    active={state.visibilityFilter === props.filter}
-                    onClick={() =>
-                        store.dispatch({
-                            type: 'SET_VISIBILITY_FILTER',
-                            filter: props.filter
-                        })
-                    }
-                >
-                    {props.children}
-                </Link>
-            );
-        }
-    }
-
-    FilterLink.contextTypes = {
-        store: React.PropTypes.object
+    const MapStateToLinkParams = (
+        state,
+        ownProps
+    ) => {
+        return {
+            active: state.visibilityFilter === ownProps.filter
+        };
     };
+    const MapDispatchToLinkParams = (
+        dispatch,
+        ownProps
+    ) => {
+        return {
+            onClick: () => {
+                dispatch({
+                    type: 'SET_VISIBILITY_FILTER',
+                    filter: ownProps.filter
+                })
+            }
+        };
+    };
+    const FilterLink = connect(
+        MapStateToLinkParams,
+        MapDispatchToLinkParams
+    )(Link);
 
     const Header = () => (
         <p>
